62831d373addbbf033fc02727a802c53f6bc24c9
Ashar123e <asharklabs+2@gmail.com>
2023-07-06 17:38:32 +0500

    Add filler words

---
+++ config/constant.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'fillerWords' => [
+        'um', 'uh', 'oh', 'er', 'ah',
+        'very', 'really', 'highly', 'like', 'just',
+        'you know', 'you see', 'right',
+        'I mean', 'I guess', 'I suppose',
+        'totally', 'literally', 'seriously'
+    ]
+];
